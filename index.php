1f8e44b64201376c6db7e36c1db80119dda63c80
Mike Lehmann <lehmann314159@gmail.com>
2015-03-05 22:51:08 -0700

    This is where I got tired.
    On page load a list is created.  When clicked a task is editable.
    
    On button click I'm not getting a successful getJSON call.  I've read
    that I can pass get parameters as an optional argument, but I can't
    seem to get it done.
    
    Oh well, at least the back end is done.

--- index.php
+++ index.php
@@ -4,26 +4,62 @@
 
 <body>
 	<script>
-	$(document).ready(function() {
+	// Gets all the tasks, then makes a list of links
+	var makeTaskList = function() {
 		$.getJSON("readAll.php", function(data) {
+			//alert(JSON.stringify(data));
 			var items = [];
 			$.each(data, function(key, val) {
-				console.log(key);
-				console.log(val);
-				items.push("<li id='" + key + "'>" + val.title + "</li>");
+				items.push("<li id='" + key + "'>" + "<a href='#' onclick='editTask(" + key + ")'>" + val.title + "</a></li>");
 			});
-	
+
 			$("<ul/>", {
-				"class": "my-new-list",
+				"class": "tasklist",
 				html: items.join("")
 			}).appendTo("body");
 		});
+	};
+
+	// gets a single task, and creates a form for it
+	// I'm reading all because I can't make the optional json query strong thing work
+	var makeTaskForm = function(id) {
+		$.getJSON("readAll.php", function(data) {
+			$form = $("<form id='task'></form>");
+			$.each(data, function(key, val) {
+				if (key == id) {
+					$form.append("<input type='hidden' name='id' value='" + val.id + "' />");
+					$form.append("Title<br>");
+					$form.append("<input name='title' value='" + val.title + "' /><br><br>");
+					$form.append("Description<br>");
+					$form.append("<input name='description' value='" + val.description + "' /><br><br>");
+					$form.append("Is Done<br>");
+					$form.append("<input name='isDone' value='" + val.isDone + "' /><br><br>");
+					$form.append("<input type='button' onclick='updateTask()' name='bob' value='Update' />");
+				}
+			});
+			$('body').append($form);
+		});
+	};
+
+	var updateTask = function() {
+		alert(JSON.stringify(data));
+		$.getJSON("updateOne.php", JSON.stringify($("form").serializeArray()), function(data) {
+		});
+		$("body").empty();
+		makeTaskList();
+	}
+
+
+	var editTask = function(id) {
+		$("body").empty();
+		makeTaskForm(id);
+	};
+
+	// Runs when the page is all loaded
+	$(document).ready(function() {
+		makeTaskList();
 	});
+
 	</script>
 
-<div id="task_list">
-	<h1>Tasks</h1>
-    <ul class="tasks">
-    </ul>
-</div>
 </body>
